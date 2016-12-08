21a92a92d9dee4500f29b53d50dcd60e7bb21350
Agent 47 <oneredlite@yahoo.com>
2016-12-07 20:45:25 -0500

    Add files via upload

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Agent1 on 12/6/16.
+//  Copyright © 2016 Agent1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Hello World!")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
